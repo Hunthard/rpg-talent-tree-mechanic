43321bdff9a743bb52878079650e8a0cce97ca1b
Artyom Okhota <shpager@gmail.com>
2022-08-11 19:35:27 +0300

    Fix incorrect abilities connection

--- Assets/Scripts/TalentTreeFeature/Runtime/TalentTreeController.cs
+++ Assets/Scripts/TalentTreeFeature/Runtime/TalentTreeController.cs
@@ -54,10 +54,7 @@ namespace Huntag.TalentTreeFeature
         {
             for (int i = 1; i < Model.Talents.Count; i++)
             {
-                if (Model.Talents[i].HasRootDirectAccess())
-                    Model.Talents[i].Unlock();
-                else
-                    Model.Talents[i].Lock();
+                Model.Talents[i].Lock();
             }
 
             UpdateView();
@@ -72,33 +69,23 @@ namespace Huntag.TalentTreeFeature
             var talents = new List<Talent>(11);
 
             talents.Add(new Talent(0, "Base", new ExploredTalentState(), 0));
-            talents.Add(new Talent(1, "1", new UnlockedTalentState(), 1, talents[0]));
-            talents.Add(new Talent(2, "2", new UnlockedTalentState(), 2, talents[0]));
-            talents.Add(new Talent(3, "3", new UnlockedTalentState(), 2, talents[2]));
-            talents.Add(new Talent(4, "4", new UnlockedTalentState(), 2, talents[0]));
-            talents.Add(new Talent(5, "5", new UnlockedTalentState(), 2, talents[4]));
-            talents.Add(new Talent(6, "6", new UnlockedTalentState(), 2, talents[4]));
-            talents.Add(new Talent(7, "7", new UnlockedTalentState(), 2, talents[5], talents[6]));
-            talents.Add(new Talent(8, "8", new UnlockedTalentState(), 2, talents[0]));
-            talents.Add(new Talent(9, "9", new UnlockedTalentState(), 2, talents[0]));
-            talents.Add(new Talent(10, "10", new UnlockedTalentState(), 2, talents[8], talents[9]));
+            talents.Add(new Talent(1, "1", new LockedTalentState(), 1, talents[0]));
+            talents.Add(new Talent(2, "2", new LockedTalentState(), 2, talents[0]/*, talents[3]*/));
+            talents.Add(new Talent(3, "3", new LockedTalentState(), 2, talents[2]));
+            talents.Add(new Talent(4, "4", new LockedTalentState(), 2, talents[0]/*, talents[5], talents[6]*/));
+            talents.Add(new Talent(5, "5", new LockedTalentState(), 2, talents[4]/*, talents[7]*/));
+            talents.Add(new Talent(6, "6", new LockedTalentState(), 2, talents[4]/*, talents[7]*/));
+            talents.Add(new Talent(7, "7", new LockedTalentState(), 2, talents[5], talents[6]));
+            talents.Add(new Talent(8, "8", new LockedTalentState(), 2, talents[0]/*, talents[10]*/));
+            talents.Add(new Talent(9, "9", new LockedTalentState(), 2, talents[0]/*, talents[10]*/));
+            talents.Add(new Talent(10, "10", new LockedTalentState(), 2, talents[8], talents[9]));
 
-            //for (int i = 1; i < talents.Capacity; i++)
-            //{
-            //    talents.Add(new Talent(i, i.ToString(), new UnlockedTalentState(), (uint)i));
-            //    talents[i].AddLinkedTalents(talents[0]);
-            //}
-
-            //talents[5].AddLinkedTalents(talents[4]);
-            //talents[5].RemoveLinckedTalents(talents[0]);
-            //talents[6].AddLinkedTalents(talents[4]);
-            //talents[6].RemoveLinckedTalents(talents[0]);
-            //talents[7].AddLinkedTalents(talents[5], talents[6]);
-            //talents[7].RemoveLinckedTalents(talents[0]);
-            //talents[3].AddLinkedTalents(talents[2]);
-            //talents[3].RemoveLinckedTalents(talents[0]);
-            //talents[10].AddLinkedTalents(talents[8], talents[9]);
-            //talents[10].RemoveLinckedTalents(talents[0]);
+            talents[2].AddLinkedTalents(talents[3]);
+            talents[2].AddLinkedTalents(talents[3]);
+            talents[4].AddLinkedTalents(talents[5], talents[6]);
+            talents[6].AddLinkedTalents(talents[7]);
+            talents[8].AddLinkedTalents(talents[10]);
+            talents[9].AddLinkedTalents(talents[10]);
 
             Model = new TalentTreeModel(talents);
         }
@@ -116,7 +103,7 @@ namespace Huntag.TalentTreeFeature
         {
             View.ExploreClicked += Explore;
             View.ResetClicked += ResetAbility;
-            
+
             foreach (var button in View.TalentButtons)
             {
                 AddButtonEventHandler(button);
@@ -138,12 +125,12 @@ namespace Huntag.TalentTreeFeature
 
         private void UpdateView()
         {
-            CheckTreeTalentsStatus();
+            UnlockAvailableTalents();
 
             if (_selectedTalent != null)
             {
-                View.Explore.gameObject.SetActive(!(_selectedTalent.State is ExploredTalentState));
-                View.Reset.gameObject.SetActive(!(_selectedTalent.State is UnlockedTalentState));
+                View.Explore.gameObject.SetActive(_selectedTalent.State is UnlockedTalentState);
+                View.Reset.gameObject.SetActive(_selectedTalent.State is ExploredTalentState);
             }
 
             for (int i = 0; i < View.TalentButtons.Count; i++)
@@ -170,11 +157,12 @@ namespace Huntag.TalentTreeFeature
             }
         }
 
-        private void CheckTreeTalentsStatus()
+        private void UnlockAvailableTalents()
         {
-            foreach (var talent in Model.Talents)
+            for (int i = 1; i < Model.Talents.Count; i++)
             {
-                talent.CheckStatus();
+                if (Model.Talents[i].State is LockedTalentState && Model.Talents[i].HasOneOrMoreExploredLinkedTalents())
+                    Model.Talents[i].Unlock();
             }
         }
 
@@ -189,8 +177,8 @@ namespace Huntag.TalentTreeFeature
             View.TalentName.text = _selectedTalent.Name;
             View.TalentDescription.text = $"Cost: {_selectedTalent.Cost}";
 
-            View.Explore.gameObject.SetActive(!(_selectedTalent.State is ExploredTalentState));
-            View.Reset.gameObject.SetActive(!(_selectedTalent.State is UnlockedTalentState));
+            View.Explore.gameObject.SetActive(_selectedTalent.State is UnlockedTalentState);
+            View.Reset.gameObject.SetActive(_selectedTalent.State is ExploredTalentState);
 
             //if (talent.State is LockedTalentState) return;
 
